fea69008b540b84e4eb3d761ee1257e80e732ce8
Larry Garfield <larry@garfieldtech.com>
2010-06-14 00:17:52 +0000

    Add templates for selected webform nodes.

---
+++ sites/default/themes/stf/webform-mail-168467.tpl.php
@@ -0,0 +1,43 @@
+<?php /* Fleet Command Email */ ?>
+  Reporter's Name: <?php echo $form_values['submitted']['name'] ?>
+  Position: <?php echo $form_values['submitted']['position'] ?>
+  
+  =/\= <? php echo $form_values['submitted']['first_ship_name'] ?> =/\=
+  
+  Overall ship health: <?php echo $form_values['submitted']['first_overall_ship_health'] ?>
+  
+  Comments: <?php echo $form_values['submitted']['first_comments'] ?> 
+  
+   =/\= <? php echo $form_values['submitted']['second_ship_name'] ?> =/\=
+  
+  Overall ship health: <?php echo $form_values['submitted']['second_overall_ship_health'] ?>
+  
+  Comments: <?php echo $form_values['submitted']['second_comments'] ?>
+
+  =/\= <? php echo $form_values['submitted']['third_ship_name'] ?> =/\=
+  
+  Overall ship health: <?php echo $form_values['submitted']['third_overall_ship_health'] ?>
+  
+  Comments: <?php echo $form_values['submitted']['third_comments'] ?>   
+  
+  =/\= <? php echo $form_values['submitted']['fourt_ship_name'] ?> =/\=
+  
+  Overall ship health: <?php echo $form_values['submitted']['fourth_overall_ship_health'] ?>
+  
+  Comments: <?php echo $form_values['submitted']['fourth_comments'] ?> 
+  
+   =/\= <? php echo $form_values['submitted']['fifth_ship_name'] ?> =/\=
+  
+  Overall ship health: <?php echo $form_values['submitted']['fifth_overall_ship_health'] ?>
+  
+  Comments: <?php echo $form_values['submitted']['fifth_comments'] ?> 
+  
+  =/\= <? php echo $form_values['submitted']['sixth_ship_name'] ?> =/\=
+  
+  General Comments: <?php echo $form_values['submitted']['general_comments'] ?>
+  
+  <?php /* End Fleet Command Email */ ?>
+  
+  
+  
+ 
